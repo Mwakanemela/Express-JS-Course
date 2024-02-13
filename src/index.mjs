8bc1344c2f1615a03b197a5506f6f6689ac7cf3b
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2024-02-13 21:56:03 +0200

    finished get requests

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,19 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 2002;
+
+app.get("/", (req, res) => {
+  res.send("Welcome, to Mwaka Web Server");
+});
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 1, username: "Mwaka", fullName: "Mwakanemela Kayange" },
+    { id: 2, username: "Mwaka", fullName: "Mwakanemela Kayange" },
+    { id: 3, username: "Mwaka", fullName: "Mwakanemela Kayange" },
+  ]);
+});
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);

4489134dece331dc0a74f4d1134a175d6bff6c02
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2024-02-13 22:36:28 +0200

    route parameters(params)

--- src/index.mjs
+++ src/index.mjs
@@ -4,18 +4,31 @@ const app = express();
 
 const PORT = process.env.PORT || 2002;
 
+const myUsers = [
+  { id: 1, username: "Mwaka", fullName: "Mwakanemela Kayange" },
+  { id: 2, username: "Mwaka", fullName: "Mwakanemela Kayange" },
+  { id: 3, username: "Mwaka", fullName: "Mwakanemela Kayange" },
+];
 app.get("/", (req, res) => {
   res.send("Welcome, to Mwaka Web Server");
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, username: "Mwaka", fullName: "Mwakanemela Kayange" },
-    { id: 2, username: "Mwaka", fullName: "Mwakanemela Kayange" },
-    { id: 3, username: "Mwaka", fullName: "Mwakanemela Kayange" },
-  ]);
+  res.send(myUsers);
 });
 
+app.get('/api/users/:id', (req, res) => {
+    console.log(req.params)
+    const parsedId = parseInt(req.params.id)
+    if (isNaN(parsedId)) {
+        return res.status(400).send({msg : "Bad Request: Invalid ID"})
+    }
+
+    const findUser = myUsers.find((user) => user.id === parsedId)
+    if (!findUser) return res.sendStatus(404)
+    return res.send(findUser)
+
+})
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
